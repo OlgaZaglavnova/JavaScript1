94cdda47cd7e61379c9e7d498f792a3161698075
OlgaZaglavnova <olga.zaglavnova@mail.ru>
2019-11-08 11:19:08 +0300

    JavaScript1 Lesson 5 Task 4. Создаются карточки товаров, количество запрашивается у пользователя.

---
+++ hometask5 task4/scripts/script.js
@@ -0,0 +1,12 @@
+'use strict';
+let init = function(){
+    let cardsCount = Number(prompt("Введите количество товаров (целое число от 1 до 100"));
+    if (cardsCount > 0 && cardsCount <101 && Number.isInteger(cardsCount)){
+        cardsCreater.createHTMLcode(cardsCount);
+        return;
+    } else{
+        alert ("Введено значение вне диапазона");
+        init();
+    }
+}
+init();
